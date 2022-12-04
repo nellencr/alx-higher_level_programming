f96ded8f0e686275ba51594beba07fc052968554
nellencr <nellencr@gmail.com>
2022-12-03 23:54:34 -0800

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    """Adds elements of 2 tuples"""
-    if len(tuple_a) < 1:
-        tuple_a = (0, 0)
-    elif len(tuple_a) < 2:
-        tuple_a = (tuple_a[0], 0)
-    if len(tuple_b) < 1:
-        tuple_b = (0, 0)
-    elif len(tuple_b) < 2:
-        tuple_b = (tuple_b[0], 0)
-    res = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return res
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
+        print()
