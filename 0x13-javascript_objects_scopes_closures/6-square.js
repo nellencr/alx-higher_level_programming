63c3427b2b6c0a03c02d8a4e2dc964dedc75a24d
NELA <nellencr@gmail.com>
2023-03-14 13:28:17 +0200

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,26 @@
 #!/usr/bin/node
+// A script that computes the number of tasks completed by user id.
 
-const SquareOne = require('./5-square');
-
-module.exports = class Square extends SquareOne {
-  charPrint (char) {
-    const c = char || 'X';
-    let a = c.repeat(this.width) + '\n';
-    a = a.repeat(this.height);
-    console.log(a.slice(0, -1));
+const args = process.argv;
+let reqURL = args[2];
+let request = require('request');
+request(reqURL, function (error, response, body) {
+  if (error) {
+    console.log('error:', error);
+  } else {
+    let todos = JSON.parse(body);
+    let dash = {};
+    for (let i = 0; i < todos.length; i++) {
+      let status = (todos[i]['completed']);
+      let key = todos[i]['userId'].toString();
+      if (status) {
+        if (dash[key]) {
+          dash[key]++;
+        } else {
+          dash[key] = 1;
+        }
+      }
+    }
+    console.log(dash);
   }
-}
+});
