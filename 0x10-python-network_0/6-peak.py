a38c7f2baf8160c7c1efa0a9c3c854c92f1095a7
NELA <nellencr@gmail.com>
2023-03-30 20:55:15 +0200

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,35 +1,42 @@
 
 
 #!/usr/bin/python3
-"""
-    Function: find_peak(listint)
-"""
+"""Module that finds a peak in a list of integers"""
 
 
-def find_peak(listint):
+def find_peak(list_of_integers):
+    """function that finds a peak in a list of unsorted integers.
+    Args:
+        list_of_integers (list): a list of integers
+    Your algorithm must have the lowest complexity.
+    Note: there may be more than one peak in the list.
+    The most naive solution to this is to just go through each element
+    one-by-one and see if it's qualified as a peak. This solution will take
+    O(n) time complexity at the worst case and O(1) for space complexity
+    which is super for most algorithm problem. So, here comes the tricky
+    part, solve it with O(log(n)) time complexity!
+    Usually Binary Search is being used in sorted array(it could also mean
+    Bitonic array, or array that's sorted in some other ways), but this
+    one is a little bit different as we can't sort the array.
+    In Binary Search, we always check the middle value and see if it's
+    qualified to be a peak, if not then we change the start or end pointer
+    so that we get a new middle value.
+    Returns:
+        int: peak(s)
     """
-        finds a peak in a list of unsorted integers
-        Args:
-            listint (list)
-        Return:
-            peak
-    """
-    listint = listint.copy()
+    list_ = list_of_integers
+    # if there is no list of integers return None
+    if list_ == []:
+        return None
+    length = len(list_)
 
-    length = len(listint)
-
-    if length == 0:
-        return
-
-    # find index of element in middle
-    mid = int(length/2)
-
-    # compare mid index element with neighbours if they exist
-    if (mid == 0 or listint[mid - 1] <= listint[mid]) and (mid == length - 1
-                                                           or listint[mid + 1]
-                                                           < listint[mid]):
-        return listint[mid]
-    elif mid > 0 and listint[mid - 1] > listint[mid]:
-        return find_peak(listint[:mid])
-    else:
-        return find_peak(listint[mid:])
+    start, end = 0, length - 1
+    while start < end:
+        mid = start + (end - start) // 2
+        if list_[mid] > list_[mid - 1] and list_[mid] > list_[mid + 1]:
+            return list_[mid]
+        if list_[mid - 1] > list_[mid + 1]:
+            end = mid
+        else:
+            start = mid + 1
+    return list_[start]
