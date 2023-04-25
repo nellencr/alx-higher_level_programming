d4b22e13072ddc5c64a36d099a6473a9b32f1a2a
NELA <nellencr@gmail.com>
2023-04-25 15:15:02 +0200

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,27 +3,21 @@
 // Display characters name in the same order of the list  characters in the film response
 
 const request = require('request');
-const filmUrl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
 
-function printCharacters (characters, idx) {
-  request(characters[idx], (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
       console.log(JSON.parse(body).name);
-      if (idx + 1 < characters.length) {
-        printCharacters(characters, idx + 1);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
       }
     }
   });
 }
-
-request(filmUrl, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const chars = JSON.parse(body).characters;
-    printCharacters(chars, 0);
-  }
-});
-
