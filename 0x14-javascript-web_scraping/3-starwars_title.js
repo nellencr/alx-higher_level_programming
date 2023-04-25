418408d25d92bfcd7c6794c73ecfff5930c4bdc0
NELA <nellencr@gmail.com>
2023-04-25 15:25:44 +0200

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,22 @@
-#!/usr/bin/node
-// A script that prints the title of a film
-
+##!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = process.argv[2];
+const characterId = '18';
+let count = 0;
 
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
     const data = JSON.parse(body);
-    console.log(data.title);
+    data.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
+          count += 1;
+        }
+      });
+    });
+    console.log(count);
   }
 });
