2a5a63a75224bd61acc9e5e5f6155eed629ce554
NELA <nellencr@gmail.com>
2023-04-25 15:07:40 +0200

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 // This a script that display the status code of a GET request
 
-const args = process.argv;
-let request = require('request');
-request(args[2], function (error, response, body) {
-  if (error) {
-    console.log('error:', error); 
-  } else console.log('code:', response && response.statusCode);
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
