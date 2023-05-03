89b7522520348d86373e545538a7c9e2a0f2b100
Nellencr <nellencr@gmail.com>
2023-05-03 12:09:00 +0200

    all tasks solutions

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+// Select the <header> element using document.querySelector
+const header = document.querySelector('header');
+
+// Update the text color of the <header> element
+header.style.color = '#FF0000';
+
